d33bdc46c0d8e9f6f7b12d0ef193b97f1d235f98
Roy Vashisht <rishavvashisht845@gmail.com>
2019-09-13 17:00:16 -0400

    First COmmit

---
+++ lesson 1 comp-2084/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using lesson_1_comp_2084.Models;
+
+namespace lesson_1_comp_2084.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "welcome to georgian college, class-comp 2084.";
+            ViewData["moreinformation"] = "THis is some more information";
+            ViewData["moreinformation1"] = "hello, is it really working";
+
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ lesson 1 comp-2084/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace lesson_1_comp_2084.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
